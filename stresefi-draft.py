28ca5912127cafac64f4d9057e8e8879040a3e7c
Normando Ali Zubia Hernández <nzubia@blacklabs.mx>
2018-03-14 18:12:07 -0700

    Hash Table generation added, initial script draft

---
+++ stresefi-draft.py
@@ -0,0 +1,42 @@
+
+class Node:
+
+    word = ""
+    occurrences = 0
+    childNodes = []
+
+    # TODO: Create constructor
+    # TODO: Redefine class
+
+if __name__ == '__main__':
+
+    # Word sequence to be split
+    input = "A,B,C,D,E,A,B,C,D"
+    word_separator = ","
+    sentence_delimiter = ""
+
+    # Create split list
+    sentence_split = input.split(sentence_delimiter) if sentence_delimiter != "" else input
+
+    # Hash table creation
+    # hash_table = set(split_list)
+    hash_table = {}
+
+    # Root Node creation
+    root_node = Node()
+
+    for sentence in sentence_split:
+        # Create split list
+        split_list = sentence.split(word_separator)
+
+        # Clear parent list
+        parent_list = []
+git 
+        for value in sentence:
+
+            # Add value to hash table
+            if value not in hash_table:
+                hash_table[value] = len(hash_table)
+            print(hash_table)
+
+    print(hash_table)
